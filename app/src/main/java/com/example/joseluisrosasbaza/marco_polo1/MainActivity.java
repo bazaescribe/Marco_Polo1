4360b8955aafe130b5af3822c54dd002681f3640
Jose Luis Rosas Baza <joseluisrosasbaza@MacBook-Pro-de-Jose.local>
2015-02-04 21:04:45 -0600

    Initial commit

---
+++ app/src/main/java/com/example/joseluisrosasbaza/marco_polo1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.joseluisrosasbaza.marco_polo1;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnOne = (Button)findViewById(R.id.buttonExplore);
+        Button btnTwo = (Button)findViewById(R.id.buttonViewCards);
+        btnOne.setOnClickListener(this);
+        btnTwo.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View viewClicked) {
+        int idView = viewClicked.getId();
+
+        switch (idView){
+            case R.id.buttonExplore:
+                Log.d(LOG_TAG,"Explorar mapa");
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.buttonViewCards:
+                Log.d(LOG_TAG,"Ver cartas recolectadas");
+                break;
+        }
+    }
+}
